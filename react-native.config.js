2346cacaddd2a737ae3bb210d34bc42a00133599
vothicamtu <22130308@st.hcmuaf.edu.vn>
2026-05-01 23:43:33 +0700

    "fix: React Native CLI version for iOS autolinking

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   dependencies: {
-    // Disable Flipper completely for React Native 0.81.4
     ...(process.env.NO_FLIPPER ? { 
       'react-native-flipper': { 
         platforms: { 
@@ -12,19 +11,5 @@ module.exports = {
       } 
     } : {}),
   },
-  assets: ['./assets/fonts/'], // Font assets
-  project: {
-    ios: {
-      sourceDir: 'ios',
-      xcodeProject: {
-        name: 'Modis.xcodeproj',
-        isWorkspace: false,
-      },
-    },
-    android: {
-      sourceDir: 'android',
-      appName: 'app',
-      packageName: 'com.modis',
-    },
-  },
+  assets: ['./assets/fonts/'],
 };
